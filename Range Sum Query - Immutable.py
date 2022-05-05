0dc90c68f665bfc77df820d7fc947865484f2658
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-05 14:34:31 -0700

    Add files via upload

--- Range Sum Query - Immutable.py
+++ Range Sum Query - Immutable.py
@@ -20,3 +20,12 @@ class NumArray:
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # param_1 = obj.sumRange(left,right)
+
+    # Solution 2
+    # def __init__(self, nums: List[int]):
+    #     self.accu = [0]
+    #     for num in nums: 
+    #         self.accu += [self.accu[-1] + num]
+
+    # def sumRange(self, left: int, right: int) -> int:
+    #     return self.accu[right + 1] - self.accu[left]
